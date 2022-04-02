942058178f1195b3f9af2df5eb78ac29d86ba480
omeresas <omer.esas@mail.polimi.it>
2022-04-02 10:17:27 +0300

    implemented PostList component

---
+++ mini-blog-app/client/src/PostList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+
+    setPosts(res.data); //axios uses res.data
+  };
+
+  // empty array of variables to check before re-rendering, fetch posts only once
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={posts.id} // because we generate list of elements, react expects key property on each element
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flew-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
+
+export default PostList;
--- mini-blog-app/notes/README.md
+++ mini-blog-app/notes/README.md
@@ -8,3 +8,4 @@ This folder contains notes that were created while implementing the simple blog 
 2. [Implement the posts app and test manually](markdown_files/02_posts_app.md)
 3. [Do the same for the comments app](markdown_files/03_comments_app.md)
 4. [Create first React component PostCreate](markdown_files/04_react_app.md)
+5. [Implement the PostList component to render posts](markdown_files/05_PostList_component.md)
